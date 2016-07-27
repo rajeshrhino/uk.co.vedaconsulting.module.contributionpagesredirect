a38ac4c5866475ac73d7865f04b4933e83e053f8
Rajesh Kumar <rajesh@Rajeshs-MacBook-Pro.local>
2016-07-27 13:54:46 +0100

    Initial commit

---
+++ contributionpagesredirect.php
@@ -0,0 +1,132 @@
+<?php
+
+require_once 'contributionpagesredirect.civix.php';
+
+// URL to redirect to from Online contribution thank you page
+define('CIVICRM_CONTRIBUTION_THANKYOU_PAGE_REDIRECT_TO', 'http://www.google.com');
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function contributionpagesredirect_civicrm_config(&$config) {
+  _contributionpagesredirect_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function contributionpagesredirect_civicrm_xmlMenu(&$files) {
+  _contributionpagesredirect_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function contributionpagesredirect_civicrm_install() {
+  _contributionpagesredirect_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function contributionpagesredirect_civicrm_uninstall() {
+  _contributionpagesredirect_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function contributionpagesredirect_civicrm_enable() {
+  _contributionpagesredirect_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function contributionpagesredirect_civicrm_disable() {
+  _contributionpagesredirect_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed
+ *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function contributionpagesredirect_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _contributionpagesredirect_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function contributionpagesredirect_civicrm_managed(&$entities) {
+  _contributionpagesredirect_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function contributionpagesredirect_civicrm_caseTypes(&$caseTypes) {
+  _contributionpagesredirect_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function contributionpagesredirect_civicrm_angularModules(&$angularModules) {
+_contributionpagesredirect_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function contributionpagesredirect_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _contributionpagesredirect_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+function contributionpagesredirect_civicrm_buildForm( $formName, &$form ) {
+  if ($formName == 'CRM_Contribute_Form_Contribution_ThankYou') {
+    CRM_Utils_System::redirect(CIVICRM_CONTRIBUTION_THANKYOU_PAGE_REDIRECT_TO);
+  }
+}
